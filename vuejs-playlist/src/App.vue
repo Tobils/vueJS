8df281607d8bddb5011c36d2c7938edef2a9080a
dev-tobil <ade.suhada@mail.ugm.ac.id>
2020-03-08 15:04:33 +0700

    vue files

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>
+        {{ title }}
+    </h1>
+    <p>
+        {{ greeting() }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        title: 'Mantul, vue file pertama'
+    }
+  }, 
+  methods: {
+    greeting: function(){
+        return 'Heyy buddy'
+    }
+  }
+}
+</script>
+
+<style>
+div {
+    margin:  0px auto;
+}
+</style>
